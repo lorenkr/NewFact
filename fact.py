8df8ffd50fa2acbe817aac943aa0059c4e4ea3df
lorenkr <lorenkr20@gmail.com>
2025-11-15 10:29:18 +0200

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import random
+
+# Create the main window
+root = tk.Tk()
+root.title("Fact Of The Day")
+root.geometry("400x300")
+
+with open("facts.txt", "r") as f:
+    facts = [line.strip() for line in f.readlines()]
+
+# Create a label widget
+label = tk.Label(root, text="My New Fact Is")
+label.pack(pady=20) # Pack the label into the window
+
+
+# Create a button widget
+def on_button_click():
+    fact=random.choice(facts)
+    label.config(text=fact)
+    button.pack_forget()
+
+
+button = tk.Button(root, text="Click To Learn", command=on_button_click)
+button.pack()
+
+# Start the main event loop
+root.mainloop()
